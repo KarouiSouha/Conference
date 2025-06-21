bad47cc30c1b0054b595eb306900fc6a765b41ef
Aya Lamouchi <lamouchieya@bizerte.r-iset.tn>
2025-06-21 01:07:20 +0100

    comites

---
+++ backend/database/migrations/2025_06_20_231546_update_committee_type_enum_in_comites_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('comites', function (Blueprint $table) {
+            $table->enum('committee_type', ['scientific', 'organizing', 'honorary'])->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('comites', function (Blueprint $table) {
+            $table->enum('committee_type', ['scientific', 'organizing'])->change();
+        });
+    }
+};
